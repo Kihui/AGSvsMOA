5a8f8fc01bacf8dd8a856229595fb7ebaf6975ba
Pernath <cgah.95@gmail.com>
2016-12-12 21:26:22 -0600

    Estructura del proyecto lista para ANT.

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import org.moeaframework.problem.tsplib.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Main{
+    public static TSPInstance problema;
+
+    public static void main(String[] argumenta){
+
+        if(argumenta.length != 1){
+            System.err.println("La vida es ruda");
+            return;
+        }
+
+        try{
+            File tsp = new File(argumenta[0]);
+            problema = new TSPInstance(tsp);
+            String s = argumenta[0].substring(0, argumenta[0].lastIndexOf('.'));
+            s += ".opt.tour";
+            File tspout = new File(s);
+            if(tspout.exists()) {
+                problema.addTour(tspout);
+                for (Tour tour : problema.getTours())
+                    System.out.println("Distancia mínima: " + tour.distance(problema));
+            }
+        } catch(IOException ioe) {
+            System.err.println("Hubo un problema al abrir el archivo TSP");
+            ioe.printStackTrace();
+        }
+	
+        if(problema == null)
+            return;
+	else
+	    System.out.println("Archivo: \'"+argumenta[0]+"\' abierto correctamente");
+    }
+}
